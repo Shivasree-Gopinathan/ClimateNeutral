089db6018492212404a75acc5fcb529b0f54c301
Jitali Hadiya <jitalihhadiya@gmail.com>
2024-02-03 22:43:13 -0500

    Updated Add records functionality

--- scripts/main.js
+++ scripts/main.js
@@ -1,28 +1,4 @@
-// script for timelog
-function downloadtable() {
-  var node = document.getElementById('demo')
-  domtoimage
-    .toPng(node)
-    .then(function (dataUrl) {
-      var img = new Image()
-      img.src = dataUrl
-      downloadURI(dataUrl, 'records.png')
-    })
-    .catch(function (error) {
-      console.error('oops, something went wrong', error)
-    })
-}
-
-function downloadURI(uri, name) {
-  var link = document.createElement('a')
-  link.download = name
-  link.href = uri
-  document.body.appendChild(link)
-  link.click()
-  document.body.removeChild(link)
-  delete link
-}
-
+// User Data Input's Add Functionality
 var description = document.getElementById('description')
 var type = document.getElementById('type')
 var year = document.getElementById('year')
@@ -38,118 +14,15 @@ var selectedrow = null
 var flag = false
 var rowIndex = 0
 
-display()
+// Dynamically generating list of years
+var currentYear = new Date().getFullYear()
+var selectYear = document.getElementById('year')
 
-function addRecord() {
-  flag = calculate()
-  if (flag === -1) {
-    return
-  }
-  let records = new Array()
-  records = JSON.parse(localStorage.getItem('record'))
-    ? JSON.parse(localStorage.getItem('record'))
-    : []
-  records.push({
-    Id: indexOf(),
-    Start_Time: document.getElementById('start-time').value,
-    End_Time: document.getElementById('end-time').value,
-    Minutes: flag,
-    Task_Description: document.getElementById('task-description').value,
-  })
-  localStorage.setItem('record', JSON.stringify(records))
-  location.reload()
-  //insert without local storage
-  // let newRow = table.insertRow(table.rows.length)
-  // let cell1 = newRow.insertCell(0)
-  // let cell2 = newRow.insertCell(1)
-  // let cell3 = newRow.insertCell(2)
-  // let cell4 = newRow.insertCell(3)
-  // let cell5 = newRow.insertCell(4)
-
-  // cell1.innerHTML = startTime.value
-  // cell2.innerHTML = endTime.value
-  // cell3.innerHTML = flag
-  // cell4.innerHTML = taskDescription.value
-  // cell5.innerHTML =
-  //     '<button  class="btn1" onclick="edit(this);">Edit</button> <button class="btn1" onclick="delete_record(this);">Delete</button>'
-  // clear()
-}
-
-function indexOf() {
-  var table = document.getElementById('table-content')
-  var tl = table.rows.length
-  return tl - 2
-}
-
-function clear() {
-  startTime.value = ''
-  endTime.value = ''
-  taskDescription.value = ''
-}
-
-function delete_record(r) {
-  // delete without local storage
-  // let i = r.parentNode.parentNode.rowIndex
-  // document.getElementById('table2').deleteRow(i)
-  if (confirm('Are you sure you want to delete this record ?')) {
-    entries = JSON.parse(localStorage.getItem('record'))
-    selectedrow = r.parentElement.parentElement
-    rowIndex = selectedrow.cells[5].innerHTML
-    for (let i = 0; i < entries.length; i++) {
-      if (entries[i].Id == rowIndex) {
-        entries.splice(i, 1)
-        location.reload()
-        break
-      }
-    }
-    for (let i = rowIndex; i < entries.length; i++) {
-      entries[i].Id = entries[i].Id - 1
-    }
-    localStorage.setItem('record', JSON.stringify(entries))
-  }
-}
-
-function edit(r) {
-  selectedrow = r.parentElement.parentElement
-  document.getElementById('start-time').value = selectedrow.cells[0].innerHTML
-  document.getElementById('end-time').value = selectedrow.cells[1].innerHTML
-  document.getElementById('task-description').value =
-    selectedrow.cells[3].innerHTML
-  rowIndex = selectedrow.cells[5].innerHTML
-}
-
-function update() {
-  flag = calculate()
-  if (flag === -1) {
-    return
-  }
-  records = JSON.parse(localStorage.getItem('record'))
-  for (let i = 0; i < records.length; i++) {
-    if (records[i].Id == rowIndex) {
-      records[i].Start_Time = document.getElementById('start-time').value
-      records[i].End_Time = document.getElementById('end-time').value
-      records[i].Minutes = flag
-      records[i].Task_Description =
-        document.getElementById('task-description').value
-    }
-  }
-  localStorage.setItem('record', JSON.stringify(records))
-}
-
-function add() {
-  if (validate() == true) {
-    if (selectedrow == null) {
-      addRecord()
-    } else {
-      update()
-      location.reload()
-    }
-    clear()
-  }
-}
-
-function load() {
-  alert('Loaded...')
+for (var i = currentYear; i >= 1950; i--) {
+  var option = document.createElement('option')
+  option.value = i
+  option.text = i
+  selectYear.add(option)
 }
 
 function validate() {
@@ -164,28 +37,34 @@ function validate() {
   var field9 = document.getElementById('f_flex').value
   var field10 = document.getElementById('quantity').value
 
-  if (feild === '') {
+  if (field1 === '') {
     alert('Enter the description')
     return false
-  } else if (feild === '') {
+  } else if (field2 === '') {
+    alert('Enter the type')
+    return false
+  } else if (field3 === '') {
+    alert('Enter the year')
+    return false
+  } else if (field4 === '') {
     alert('Enter the make')
     return false
-  } else if (feild === '') {
+  } else if (field5 === '') {
     alert('Enter the model')
     return false
-  } else if (feild === '') {
+  } else if (field6 === '') {
     alert('Enter the annual vkt')
     return false
-  } else if (feild === '') {
+  } else if (field7 === '') {
     alert('Enter the annual fuel')
     return false
-  } else if (feild === '') {
+  } else if (field8 === '') {
     alert('Enter the fuel type')
     return false
-  } else if (feild === '') {
+  } else if (field9 === '') {
     alert('Enter the fuel flex')
     return false
-  } else if (feild === '') {
+  } else if (field10 === '') {
     alert('Enter the quantity')
     return false
   } else {
@@ -225,11 +104,12 @@ function display() {
 }
 
 function showPopUp() {
-  document.getElementById('tableTabModal').style.display = 'flex'
+  document.getElementById('tableTabform').style.display = 'flex'
 }
 
 function closePopUp() {
-  document.getElementById('tableTabModal').style.display = 'none'
+  document.getElementById('tableTabform').style.display = 'none'
+  clearForm()
 }
 
 function submitForm() {
@@ -243,6 +123,10 @@ function submitForm() {
   var f_type = document.getElementById('f_type').value
   var f_flex = document.getElementById('f_flex').value
   var quantity = document.getElementById('quantity').value
+
+  if (!validate()) {
+    return
+  }
 
   // Check for the null records
   var nullRecordsRow = document.getElementById('null-records')
@@ -273,4 +157,44 @@ function submitForm() {
 
   closePopUp()
   alert('Data added to the table!')
+  clearForm()
+}
+
+function validate() {
+  var fields = [
+    'description',
+    'type',
+    'year',
+    'make',
+    'model',
+    'a_vkt',
+    'a_fuel',
+    'f_type',
+    'f_flex',
+    'quantity',
+  ]
+
+  for (var i = 0; i < fields.length; i++) {
+    var value = document.getElementById(fields[i]).value.trim()
+    if (value === '') {
+      alert('Please fill in all fields. Field: ' + fields[i])
+      console.error('Validation failed for field: ' + fields[i])
+      return false
+    }
+  }
+
+  return true
+}
+
+function clearForm() {
+  document.getElementById('description').value = ''
+  document.getElementById('type').value = ''
+  document.getElementById('year').selectedIndex = 0
+  document.getElementById('make').value = ''
+  document.getElementById('model').value = ''
+  document.getElementById('a_vkt').value = ''
+  document.getElementById('a_fuel').value = ''
+  document.getElementById('f_type').value = ''
+  document.getElementById('f_flex').selectedIndex = 0
+  document.getElementById('quantity').value = ''
 }
