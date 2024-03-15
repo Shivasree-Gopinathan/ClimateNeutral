4ea445744adb4ca6b10cab99a8e0b585270ad101
Jitali24 <jitalihhadiya@gmail.com>
2024-03-15 14:59:21 -0400

    Changes to graph value

--- scripts/emissionIntensity.js
+++ scripts/emissionIntensity.js
@@ -1,8 +1,10 @@
-document.querySelectorAll('.hover-right').forEach(button => {
-  button.addEventListener('click', function() {
-    document.querySelector('.car').classList.add('move-once');
-  });
-});var FuelEmissionsCoefficient = 0
+document.querySelectorAll('.hover-right').forEach((button) => {
+  button.addEventListener('click', function () {
+    document.querySelector('.car').classList.add('move-once')
+  })
+})
+
+var FuelEmissionsCoefficient = 0
 
 var fuelEmissionsCoefficient = {
   Gasoline: 2299,
@@ -16,138 +18,124 @@ document.addEventListener('DOMContentLoaded', function () {
 
 //Calculating Emission Intensity
 function calculateEmissionIntensity() {
-  // console.log('Inside calculateEmissionIntensity')
-
   var table = document.getElementById('table-content')
   var tbody = table.querySelector('tbody')
   var rows = tbody.querySelectorAll('tr:not(.table-tab):not(.table-input)')
-  // console.log('Number of rows found::', rows.length)
 
   var annualEmissionsData = []
   var emissionsIntensityData = []
 
   rows.forEach(function (row, index) {
-    // console.log('Row::', index + 1)
-
     var cells = row.querySelectorAll('td')
     var annualFuelConsumption = cells[6].textContent.trim()
     var annualVKT = cells[5].textContent.trim()
     var fuelType = cells[7].textContent.trim()
-    var type = cells[1].textContent.trim()
+    var description = cells[0].textContent.trim().slice(0, 15) // Limit to 15 characters
 
     var fuelEfficiency = annualFuelConsumption / annualVKT
     var annualEmissions =
       annualFuelConsumption * fuelEmissionsCoefficient[fuelType]
     var emissionsIntensity = annualEmissions / annualVKT
-    console.log('Emissions Intensity::', emissionsIntensity)
-	selectedOptions[index + 1].annualEmissions = annualEmissions
-    selectedOptions[index + 1].emissionsIntensity = emissionsIntensity
-    console.log('New selectedOptions::')
-    console.log(selectedOptions)
-    annualEmissionsData.push({ label: type, value: annualEmissions })
-    emissionsIntensityData.push({ label: type, value: emissionsIntensity })
-    // console.log(
-    //   'Feul Efficieny::',
-    //   fuelEfficiency,
-    //   'Fuel Type::',
-    //   fuelType,
-    //   'Fuel Annual Emission::',
-    //   annualEmissions,
-    //   'Emissions Intensity::',
-    //   emissionsIntensity
-    // )
+
+    annualEmissionsData.push({ label: description, value: annualEmissions })
+    emissionsIntensityData.push({
+      label: description,
+      value: emissionsIntensity,
+    })
   })
-  // Call a function to draw the chart
-  drawHorizontalBarChart('emissionChart', annualEmissionsData)
-  drawHorizontalBarChart('emissionIntensityChart', emissionsIntensityData)
-  // drawHorizontalBarChart(
-  //   'emissionChart',
-  //   annualEmissionsData,
-  //   annualEmissionsData.map((item) => item.label)
-  // )
-  // drawHorizontalBarChart(
-  //   'emissionIntensityChart',
-  //   emissionsIntensityData,
-  //   emissionsIntensityData.map((item) => item.label)
-  // )
+
+  drawHorizontalBarChart('emissionChart', annualEmissionsData, 'value')
+  drawHorizontalBarChart(
+    'emissionIntensityChart',
+    emissionsIntensityData,
+    'emissionIntensity'
+  )
 }
 
-// function drawHorizontalBarChart(canvasId, data) {
-//   var canvas = document.getElementById(canvasId)
-//   var ctx = canvas.getContext('2d')
+function drawHorizontalBarChart(canvasId, data, valueType) {
+  var canvas = document.getElementById(canvasId)
+  var ctx = canvas.getContext('2d')
 
-//   var requiredHeight = data.length * 75 // Adjust the factor (50) as needed
+  var requiredHeight = data.length * 75
+  canvas.height = requiredHeight
 
-//   // Set the canvas height dynamically
-//   canvas.height = requiredHeight
+  ctx.clearRect(0, 0, canvas.width, canvas.height)
 
-//   // Clear the canvas
-//   ctx.clearRect(0, 0, canvas.width, canvas.height)
+  var maxValue = Math.max(...data.map((item) => item.value))
 
-//   // Calculate the maximum value for scaling
-//   var maxValue = Math.max(...data.map((item) => item.value))
+  var barHeight = 40
+  var horizontalGap = 160
+  var verticalGap = 30
 
-//   // Bar height and gap settings
-//   var barHeight = 40
-//   var horizontalGap = 100 // Horizontal gap between bars
-//   var verticalGap = 30 // Vertical gap between caption and bars
+  var animationSpeed = 3
+  var animationInterval = 10
 
-//   // Animation settings
-//   var animationSpeed = 3 // Increase to speed up animation
-//   var animationInterval = 10 // Milliseconds between animation frames
+  var barColor = canvasId === 'emissionChart' ? '#0c1081' : '#26b170'
+  var borderColor = canvasId === 'emissionChart' ? '#26b170' : '#0c1081'
 
-//   var barColor = canvasId === 'emissionChart' ? '#0c1081' : '#26b170'
+  ctx.font = 'bold 20px Lato, sans-serif'
 
-//   // Set label font size and weight
-//   ctx.font = 'bold 20px Lato, sans-serif' // Adjust font size and family as needed
+  data.forEach(function (item, index) {
+    var startX = horizontalGap - 155
+    var startY =
+      (index + 1) * verticalGap + index * barHeight + barHeight / 2 + 6
+    ctx.textAlign = 'left'
+    ctx.fillStyle = barColor
+    ctx.fillText(item.label, startX, startY) // Use description as label
 
-//   // Draw labels
-//   data.forEach(function (item, index) {
-//     var startX = horizontalGap - 95 // Adjust startX for left alignment
-//     var startY =
-//       (index + 1) * verticalGap + index * barHeight + barHeight / 2 + 6 // Adjust startY for vertical alignment
-//     ctx.textAlign = 'left'
-//     ctx.fillStyle = barColor
-//     ctx.fillText('Vehicle ' + (index + 1), startX, startY)
-//   })
+    var targetWidth =
+      (item.value / maxValue) * (canvas.width - 2 * horizontalGap)
+    var currentWidth = 0
+    var animation = setInterval(function () {
+      ctx.clearRect(
+        horizontalGap,
+        startY - barHeight / 2,
+        currentWidth,
+        barHeight
+      )
 
-//   // Draw bars horizontally with animation
-//   data.forEach(function (item, index) {
-//     var targetWidth =
-//       (item.value / maxValue) * (canvas.width - 2 * horizontalGap)
-//     var currentWidth = 0
-//     var startX = horizontalGap
-//     var startY = (index + 1) * verticalGap + index * barHeight + barHeight / 2 // Adjust startY to center vertically
+      currentWidth += animationSpeed
 
-//     // Animate the bars
-//     var animation = setInterval(function () {
-//       ctx.clearRect(startX, startY - barHeight / 2, currentWidth, barHeight)
+      ctx.fillStyle = barColor
+      ctx.fillRect(
+        horizontalGap,
+        startY - barHeight / 2,
+        currentWidth,
+        barHeight
+      )
+      ctx.strokeStyle = borderColor
+      ctx.strokeRect(
+        horizontalGap,
+        startY - barHeight / 2,
+        currentWidth,
+        barHeight
+      )
 
-//       currentWidth += animationSpeed
+      if (currentWidth >= targetWidth) {
+        clearInterval(animation)
+        // Display value with units after animation completes
+        ctx.fillStyle = barColor
+        ctx.textAlign = 'left'
 
-//       // Draw the bar
-//       ctx.fillStyle = barColor
-//       ctx.fillRect(startX, startY - barHeight / 2, currentWidth, barHeight)
-
-//       // Stop the animation when reaching the target width
-//       if (currentWidth >= targetWidth) {
-//         clearInterval(animation)
-//       }
-//     }, animationInterval)
-//   })
-// }
-
-// function toggleVisibility() {
-//   var totalEmissionsTitle = document.getElementById('totalEmissionsTitle')
-//   var emissionIntensityTitle = document.getElementById('emissionIntensityTitle')
-//   var emissionIntensityDiv = document.getElementById('EmissionIntensity')
-//   var greenOption = document.getElementById('green-option')
-
-//   totalEmissionsTitle.style.display = 'block'
-//   emissionIntensityTitle.style.display = 'block'
-//   emissionIntensityDiv.style.display = 'block'
-//   greenOption.style.display = 'none'
-// }
+        if (valueType === 'value') {
+          // For annual emissions
+          ctx.fillText(
+            (item.value / 1000).toFixed(2) + ' kgCO2e',
+            horizontalGap + currentWidth + 5,
+            startY + 6
+          )
+        } else if (valueType === 'emissionIntensity') {
+          // For emission intensity
+          ctx.fillText(
+            item.value.toFixed(2) + ' gCO2e/km',
+            horizontalGap + currentWidth + 5,
+            startY + 6
+          )
+        }
+      }
+    }, animationInterval)
+  })
+}
 
 function toggleVisibility() {
   var totalEmissionsTitle = document.getElementById('totalEmissionsTitle')
@@ -161,67 +149,4 @@ function toggleVisibility() {
   emissionIntensityDiv.style.display = 'block'
   greenOption.style.display = 'none'
   dataEntry.style.display = 'none'
-}
-
-function drawHorizontalBarChart(canvasId, data) {
-  var canvas = document.getElementById(canvasId)
-  var ctx = canvas.getContext('2d')
-
-  var requiredHeight = data.length * 75 // Adjust the factor (75) as needed
-
-  canvas.height = requiredHeight
-
-  ctx.clearRect(0, 0, canvas.width, canvas.height)
-
-  var maxValue = Math.max(...data.map((item) => item.value))
-
-  var barHeight = 40
-  var horizontalGap = 100
-  var verticalGap = 30
-
-  var animationSpeed = 3
-  var animationInterval = 10
-
-  var barColor = canvasId === 'emissionChart' ? '#0c1081' : '#26b170'
-  var borderColor = canvasId === 'emissionChart' ? '#26b170' : '#0c1081'
-
-  ctx.font = 'bold 20px Lato, sans-serif'
-
-  data.forEach(function (item, index) {
-    var startX = horizontalGap - 95
-    var startY =
-      (index + 1) * verticalGap + index * barHeight + barHeight / 2 + 6
-    ctx.textAlign = 'left'
-    ctx.fillStyle = barColor
-    ctx.fillText('Vehicle ' + (index + 1), startX, startY)
-  })
-
-  data.forEach(function (item, index) {
-    var targetWidth =
-      (item.value / maxValue) * (canvas.width - 2 * horizontalGap)
-    var currentWidth = 0
-    var startX = horizontalGap
-    var startY = (index + 1) * verticalGap + index * barHeight + barHeight / 2
-
-    var percentage = ((item.value / maxValue) * 100).toFixed(2) + '%'
-
-    var animation = setInterval(function () {
-      ctx.clearRect(startX, startY - barHeight / 2, currentWidth, barHeight)
-
-      currentWidth += animationSpeed
-
-      ctx.fillStyle = barColor
-      ctx.fillRect(startX, startY - barHeight / 2, currentWidth, barHeight)
-      ctx.strokeStyle = borderColor
-      ctx.strokeRect(startX, startY - barHeight / 2, currentWidth, barHeight)
-
-      if (currentWidth >= targetWidth) {
-        clearInterval(animation)
-        // Display percentage value after animation completes
-        ctx.fillStyle = barColor
-        ctx.textAlign = 'left'
-        ctx.fillText(percentage, startX + currentWidth + 5, startY + 6)
-      }
-    }, animationInterval)
-  })
 }
